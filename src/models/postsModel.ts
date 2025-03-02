c7a19587eed5aa121f2d37cac3e86328e4203053
Michalikhol <michali.khol@gmail.com>
2025-03-02 22:24:38 +0200

    added create dir script and changed getAll posts

--- src/models/postsModel.ts
+++ src/models/postsModel.ts
@@ -1,9 +1,14 @@
-import { Schema, model } from 'mongoose';
+import mongoose, { Schema, model } from 'mongoose';
 
 export interface IPost {
   title: string;
-  sender: string;
+  postedBy: mongoose.Schema.Types.ObjectId;
   content: string;
+  image?: string;
+  rating: number;
+  commentsCount: number;
+  likesCount: number;
+  createdAt?: Date;
 }
 
 const postSchema = new Schema<IPost>({
@@ -11,11 +16,34 @@ const postSchema = new Schema<IPost>({
     type: String,
     required: true,
   },
-  sender: {
+  postedBy: {
     type: String,
+    ref: 'Users',
     required: true,
   },
   content: String,
+  image: {
+    type: String,
+    required: false,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    default: 0,
+  },
+  commentsCount: {
+    type: Number,
+    default: 0,
+  },
+  likesCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const postModel = model<IPost>('posts', postSchema);
