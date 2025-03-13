52885e0d4b19108fd1ca601f4644096231ade4a3
Michalikhol <michali.khol@gmail.com>
2025-03-13 20:12:53 +0200

    added create like

--- src/controllers/likesController.ts
+++ src/controllers/likesController.ts
@@ -2,11 +2,33 @@ import status from 'http-status';
 import { Request, Response } from 'express';
 import likeModal, { ILike } from '../models/likesModal';
 import BaseController from './baseController';
-import commentModel from '../models/commentsModel';
+import postModel from '../models/postsModel';
 
 class LikesController extends BaseController<ILike> {
   constructor() {
     super(likeModal);
+  }
+
+  async create(req: Request, res: Response) {
+    try {
+      const postId = req.body.postId;
+
+      if (postId) {
+        const post = await postModel.findById(postId);
+        if (!post) {
+          res.status(status.NOT_FOUND).send('Post not found');
+          return;
+        }
+      }
+      await super.create(req, res);
+      if (postId) {
+        await postModel.findByIdAndUpdate(postId, {
+          $inc: { likesCount: 1 },
+        });
+      }
+    } catch (error) {
+      res.status(status.BAD_REQUEST).send(error);
+    }
   }
 }
 
